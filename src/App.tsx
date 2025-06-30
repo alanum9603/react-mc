cb4b53ac039e597ec8e7718d94428f42ce1235b1
Alanum9603 <Alanum9603@gmail.com>
2025-06-29 20:11:35 -0500

    first commit, interfaz terminada, se puede mover en el ambiente

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import { Canvas } from '@react-three/fiber'
+import { Sky } from '@react-three/drei'
+import { Physics } from '@react-three/cannon'
+import Ground from './components/Ground'
+import FPV from './components/FPV'
+import Player from './components/Player'
+
+function App() {
+  return (
+    <Canvas>
+      <Sky sunPosition={[100, 100, 100]}/>
+      <ambientLight intensity={1} />
+      <FPV />
+      <Physics>
+        <Player />
+        <Ground />
+      </Physics>
+    </Canvas>
+  )
+}
+export default App
---
+++ src/hooks/useKeyboard.ts
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react"
+const ACTIONS_KEYBOARD_MAP = {
+  KeyW:   'moveForward',
+  KeyS:   'moveBackward',
+  KeyA:   'moveLeft',
+  KeyD:   'moveRight',
+  Space:  'jump',
+  Digit1: 'dirt',
+  Digit2: 'grass',
+  Digit3: 'glass',
+  Digit4: 'wood',
+  Digit5: 'log'
+}
+
+export const useKeyboard = () => {
+  const [actions, setActions] = useState({
+    moveForward:  false,
+    moveBackward: false,
+    moveLeft:     false,
+    moveRight:    false,
+    jump:         false,
+    dirt:         false,
+    grass:        false, 
+    wood:         false,
+    log:          false
+  })
+
+  useEffect(() => {
+    
+  })
+}
+
+export default useKeyboard
